e4909f2cfa92ec907ba6eccac034a13957d3512f
Piotr Komborski <piotr.komborski@gmail.com>
2016-04-25 10:20:47 +0100

    RDS broker test
    This test checks if RDS broker is registered and listed in cf
    marketplace.

--- tests/acceptance-tests/src/acceptance/rds_broker_test.go
+++ tests/acceptance-tests/src/acceptance/rds_broker_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 )
 
